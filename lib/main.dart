81981ff86dea51b944dd14ce85380f616d79f250
AnasKhan1506 <akanaskhan1506@gmail.com>
2023-08-06 14:03:47 -0700

    Lottery App

---
+++ lib/main.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Random random = Random();
+  int x = 0;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Lottery App"),
+          ),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Center(
+                child: Text('Lottery winning number is 4'),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 250,
+                width: 300,
+                decoration: BoxDecoration(
+                  color: x == 4 ? Colors.teal : Colors.grey.withOpacity(.3),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: x == 4
+                      ? Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            const Icon(
+                              Icons.done_all,
+                              color: Colors.green,
+                              size: 35,
+                            ),
+                            const SizedBox(
+                              height: 15,
+                            ),
+                            Text(
+                              'Congratulations You have won the Lottery,your number is $x',
+                              textAlign: TextAlign.center,
+                            ),
+                          ],
+                        )
+                      : Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            const Icon(
+                              Icons.error,
+                              color: Colors.red,
+                              size: 35,
+                            ),
+                            const SizedBox(
+                              height: 15,
+                            ),
+                            Text(
+                              'Better Luck next time your number is $x\n try again',
+                              textAlign: TextAlign.center,
+                            ),
+                          ],
+                        ),
+                ),
+              )
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              x = random.nextInt(6);
+              print(x);
+              setState(() {});
+            },
+            child: const Icon(Icons.refresh),
+          )),
+    );
+  }
+}
